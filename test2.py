57a7fee0150555e48d7b35b636e31a0a1396c1a8
Jay <jay3.citrusbug@gmail.com>
2022-11-02 16:50:45 +0530

    2-11

--- test2.py
+++ test2.py
@@ -1,13 +1,13 @@
 
-# un=[]
-# random_puzzle = [
-#     ["$", "#", "#", "$", "#"], 
-#     ["%", "%", "%", "#", "#"], 
-#     ["$", "$", "%", "$", "%"]
-#     ]
-# for i in random_puzzle:
-#     for j in i:
-#         if j not in un:
-#             un.append(j)
+un=[]
+random_puzzle = [
+    ["$", "#", "#", "$", "#"], 
+    ["%", "%", "%", "#", "#"], 
+    ["$", "$", "%", "$", "%"]
+    ]
+for i in random_puzzle:
+    for j in i:
+        if j not in un:
+            un.append(j)
       
-# print(un)
+print(un)
