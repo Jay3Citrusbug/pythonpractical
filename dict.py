65e6d6d8c1ee236d73fc3833b376058988b4cded
Jay <jay3.citrusbug@gmail.com>
2022-11-08 18:16:46 +0530

    8-11

--- dict.py
+++ dict.py
@@ -1,22 +1,31 @@
 import csv
-csv_columns = ['No','Name','Country']
-dict_data = [
-{'No': 1, 'Name': 'jay', 'Country': 'India'},
-{'No': 2, 'Name': 'Bean', 'Country': 'USA'},
-{'No': 3, 'Name': 'sagar', 'Country': 'India'},
-{'No': 4, 'Name': 'Smith', 'Country': 'USA'},
-{'No': 5, 'Name': 'Yuva Raj', 'Country': 'India'},
-]
+
+# csv_columns = ['Yes','Name','Country']
+# dict_data = [
+# {'No': 1, 'Name': 'jay', 'Country': 'India'},
+# {'No': 2, 'Name': 'Bean', 'Country': 'USA'},
+# {'No': 3, 'Name': 'sagar', 'Country': 'India'},
+# {'No': 4, 'Name': 'Smith', 'Country': 'USA'},
+# {'No': 5, 'Name': 'Yuva Raj', 'Country': 'India'},
+# ]
 csv_file = "Names.csv"
 
 
 
-try:
-    with open(csv_file, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-        writer.writeheader()
-        for data in dict_data:
-            writer.writerow(data)
+# try:
+#     with open(csv_file, 'w') as csvfile:
+#         writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+#         writer.writeheader()
+#         for data in dict_data:
+#             writer.writerow(data)
             
-except IOError:
-    print("I/O error")
+# except IOError:
+#     print("I/O error")
+
+
+csv_filename = "my_file.csv"
+with open(csv_filename) as f:
+    reader = csv.DictReader(f)
+    a=(list(reader))
+    for row in a:
+        print(row)
